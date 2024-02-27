4b88e728c2f0da2ddad2be6f12657b43e8c9dc8c
anas-raza~ <anusraza268@gmail.com>
2024-02-27 11:40:24 -0800

    js file created

---
+++ assigment/no 01/script.js
@@ -0,0 +1,19 @@
++
+Addition
+-
+subraction
+*
+multiplication
+**
+Exponentiation
+/
+divison
+%
+Modulus(remainder)
+++
+Increment
+--Decrement
+alert
+
+var  x =+prompt(" + ");
+console.log("10+5");

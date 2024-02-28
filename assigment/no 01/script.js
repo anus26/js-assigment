4496d20cf2e322a5b827da669768b4f1e6c8e5be
anas-raza~ <anusraza268@gmail.com>
2024-02-27 18:14:42 -0800

    modified

--- assigment/no 01/script.js
+++ assigment/no 01/script.js
@@ -1,19 +1,30 @@
-+
-Addition
--
-subraction
-*
-multiplication
-**
-Exponentiation
-/
-divison
-%
-Modulus(remainder)
-++
-Increment
---Decrement
-alert
+// +
+// Addition
+// -
+// subraction
+// *
+// multiplication
+// **
+// Exponentiation
+// /
+// divison
+// %
+// Modulus(remainder)
+// ++
+// Increment
+// --Decrement
+// alert ("hello world");
 
-var  x =+prompt(" + ");
-console.log("10+5");
+// // var  x = ("10");
+// // var y= ("5");
+// // console.log(x+y);
+// console.log('helloworld');
+// var name = 'anusraza';
+// console.log(name)
+
+alert ('hello world');
+var num1 = '10';
+var num2= '10';
+var num3 = '10';
+console.log(typeof(num1))
+console.log(num1 + num2 + num3)

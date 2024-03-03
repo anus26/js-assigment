17f50e390a6ea3b7a081bcb9d3ec599c77c05976
anas-raza~ <anusraza268@gmail.com>
2024-03-03 11:10:10 -0800

    modified

--- assigment/no 01/script.js
+++ assigment/no 01/script.js
@@ -22,9 +22,26 @@
 // var name = 'anusraza';
 // console.log(name)
 
-alert ('hello world');
-var num1 = '10';
-var num2= '10';
-var num3 = '10';
-console.log(typeof(num1))
-console.log(num1 + num2 + num3)
+// alert ('hello world');
+// var num1 = '10';
+// var num2= '10';
+// var num3 = '10';
+// console.log(typeof(num1))
+// console.log(num1 + num2')
+
+
+
+var head=document.querySelector('.head');
+var paragraph=document.querySelector("#para");
+console.log(paragraph.innerHTML);
+
+function changeText(){
+    if(paragraph.innerHTML ==='changed through js '&& head.innerHTML === 'changed through js'){
+paragraph.innerHTML ='pakistan zindabad';
+head.innerHTML = 'hi'
+    }else{
+        paragraph.innerHTML ='changed through js'
+        head.innerHTML ='changed through js'
+    }
+}
+

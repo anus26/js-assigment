b6b06263d8bfde5106010d9ca2d61658eb3d3974
anas-raza~ <anusraza268@gmail.com>
2024-05-15 12:44:34 -0700

    modified

--- assigment/no 01/script.js
+++ assigment/no 01/script.js
@@ -115,22 +115,134 @@
 //     }
 // }
 
-const sales=['comb','pin' ,'clips', 'fancyitems',  'elastic','azharband','hanger'];
-const input=document.querySelector('#input');
+// const sales=['comb','pin' ,'clips', 'fancyitems',  'elastic','azharband','hanger'];
+// const input=document.querySelector('#input');
 
-function submit(){
-    console.log(input.value);
-    if(sales.includes(input.value)){
-        console.log('items is avalible');
+// function submit(){
+//     console.log(input.value);
+//     if(sales.includes(input.value)){
+//         console.log('items is avalible');
 
-    }else{
-        console.log('not avalible')
+//     }else{
+//         console.log('not avalible')
 
-    }
+//     }
+// }
+
+// const userInput =+prompt('enter table number');
+// const times =+prompt('times')
+//     for (let i = 1; i <= times ; i++){
+//         console.log(`${userInput}* ${i}=${userInput}`);
+// //     }
+//     for(let i =1;  i  >0; i++){
+//          console.log('i')
+//          }
+
+// document.write('qulification');
+// const education=['hsc','ssc','b.com','m.phil','phd']
+// const ul =document.querySelector('o1')
+// console.log(education);
+// for (let i= 0; i<education.length; i++){
+//     ul.innerHTML +=( `<li>${education[i]}</li>`)
+// }
+
+
+
+
+
+// anArray = new int[1o];
+// int[] anArray =
+//     100.200
+// }
+const boolean=document.querySelector('#input')
+function button(){
+    console.log(input)
+    if(10<9){
+        console.log('false')
+}else
+console.log('true')
 }
 
 
+const browserType = "mozilla";
+
+if (browserType.includes("zilla")) {
+  console.log("Found zilla!");
+} else {
+  console.log("No zilla here!");
+}
+
+
+// var head = document.querySelector('.head');
+// var paragraph = document.querySelector("#para");
+// console.log(paragraph.innerHTML)
+
+// function changeText(){
+
+//     if(paragraph.innerHTML === 'changed through js' && head.innerHTML === 'changed through js'){
+//         paragraph.innerHTML = 'lorem ipsum';
+//         head.innerHTML = 'hello world'
+//     }else{
+        // paragraph.innerHTML = 'changed through js'
+        // head.innerHTML = 'changed through js'
+    
+// }
+
+// console.log(10=='10');
+// console.log(10===11);
+
+
+
+//   const qulification=['bbc','b.com','matric']
+// console.log(includes.value )
+// console.log(qulification)
+
+
+
+
+// const stringArray=['2',4,5]
+// const numberArray=[]
+// length=stringArray.length
+
+// for (var i = 0; i < length; i++)
+// numberArray.push(parseInt(stringArray[i]));
+// console.log(numberArray)
+
+// const stringArray=['4',7,8];
+// const numberArray=[]
+// length=stringArray.length
+// for ( var i = 0; i < length; i++){
+//   let value;
+//   if(typeof stringArray[i]=== 'string'){
+//     value=parseInt(stringArray[i]  )
+//   }else{
+//     value=stringArray[i]
+//   }
+//   numberArray.push(value);
+// }
+// console.log(numberArray)
+
+
+
+
+const input=document.querySelector("input")
+const education=['SSC','HSC','BSC','BS','BCOM','MS','M.Phil','PhD']
+const arr=[]
+function button() {
+  if (education != null && education.length > 0 ) {
+    for (let i = 0; i < education.length; i++) {
+      console.log(education[i]);
+    }
+      
+    }else{
+      console.log('no education');
+    }
     
 
-
+  input.innerHTML+=`
+  
+  `
+  // console.log(education);
+  
+}
 
